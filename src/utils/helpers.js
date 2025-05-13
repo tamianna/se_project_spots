8f471b96db93257e82ca9918a87380be61066184
Tami Hernandez <tamianna.h@gmail.com>
2025-05-13 10:44:54 -0500

    feat: create handleSubmitButton function, in helpers.js.

--- src/utils/helpers.js
+++ src/utils/helpers.js
@@ -6,3 +6,21 @@ export function setButtonText(
 ) {
   button.textContent = isLoading ? loadingText : defaultText;
 }
+
+export function handleSubmitButton(request, evt, loadingText = "Saving...") {
+  evt.preventDefault();
+
+  const sumbitButton = evt.submitter;
+  const initialText = sumbitButton.textContent;
+
+  setButtonText(true, sumbitButton, initialText, loadingText);
+
+  request()
+  .then(() => {
+    evt.target.reset();
+  })
+  .catch(console.error)
+  .finally(() => {
+    setButtonText(false, sumbitButton, initialText);
+  });
+}
